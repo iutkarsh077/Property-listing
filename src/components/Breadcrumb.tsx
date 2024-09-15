d6ab0d82949c431f0bfd8439f9e059b7fae8dc6b
AkshatJain481 <akshatjain481@gmail.com>
2024-09-16 00:31:14 +0530

    pushing ui changes

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -2,7 +2,7 @@ const Breadcrumb = () => {
 
   return (
     <nav
-      className="flex mx-12 px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
+      className="shadow-md flex mx-12 px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
       aria-label="Breadcrumb"
     >
       <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
--- src/components/Tabs/Amenities.tsx
+++ src/components/Tabs/Amenities.tsx
@@ -1,9 +1,86 @@
+import AmenityCard from "../AmenityCard";
+import Gaurd from "../../assets/guard.png";
+import Electricity from "../../assets/eco-house.png";
+import Forest from "../../assets/forest.png";
+import SwimmingPool from "../../assets/swimming-pool.png";
+import People from "../../assets/ancestors.png";
 const Amenities = () => {
   return (
-    <div>
-      
+    <div className="my-8 gap-8 mx-4 grid grid-cols-4">
+      <AmenityCard
+        imageUrl={Gaurd}
+        title="24x7 security provided"
+        name="Security"
+        number={"01"}
+      />
+      <AmenityCard
+        imageUrl={Electricity}
+        title="No power cuts"
+        name="Electricity"
+        number={"02"}
+      />
+      <AmenityCard
+        imageUrl={Forest}
+        title="Area is clean and between nature"
+        name="Nice view"
+        number={"03"}
+      />
+      <AmenityCard
+        imageUrl={SwimmingPool}
+        title="Swimming pool is available"
+        name="Swimming pool"
+        number={"04"}
+      />
+      <AmenityCard
+        imageUrl={People}
+        title="Nice neighbourhood and friendly people"
+        name="Nice neighbourhood"
+        number={"05"}
+      />
+      <AmenityCard
+        imageUrl={Electricity}
+        title="No power cuts"
+        name="Electricity"
+        number={"06"}
+      />
+      <AmenityCard
+        imageUrl={Forest}
+        title="Area is clean and between nature"
+        name="Nice view"
+        number={"07"}
+      />
+      <AmenityCard
+        imageUrl={SwimmingPool}
+        title="Swimming pool is available"
+        name="Swimming pool"
+        number={"08"}
+      />
+      <AmenityCard
+        imageUrl={Gaurd}
+        title="24x7 security provided"
+        name="Security"
+        number={"09"}
+      />
+      <AmenityCard
+        imageUrl={Electricity}
+        title="No power cuts"
+        name="Electricity"
+        number={"10"}
+      />
+      <AmenityCard
+        imageUrl={Forest}
+        title="Area is clean and between nature"
+        name="Nice view"
+        number={"11"}
+      />
+      <AmenityCard
+        imageUrl={SwimmingPool}
+        title="Swimming pool is available"
+        name="Swimming pool"
+        number={"12"}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Amenities
+export default Amenities;
